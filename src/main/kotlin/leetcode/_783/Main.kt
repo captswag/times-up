853dfe170fa8400345e0264d8ed3cc10979952d7
captswag <iamanjithsasindran@gmail.com>
2022-01-05 16:19:00 +0530

    Leetcode - 783. Minimum Distance Between BST Nodes

---
+++ src/main/kotlin/leetcode/_783/Main.kt
@@ -0,0 +1,24 @@
+package leetcode._783
+
+import utils.TreeUtils
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    print("Enter the size of the Binary Search Tree: ")
+    val size = scanner.nextInt()
+
+    val nums = IntArray(size)
+    for (index in nums.indices) {
+        print("Enter the value at index $index: ")
+        nums[index] = scanner.nextInt()
+    }
+
+    val root = TreeUtils.createFromIntArray(nums)
+
+    val solution = Solution()
+    val output = solution.minDiffInBST(root)
+
+    println("The minimum difference between the values of any two different nodes in the tree is: $output")
+}
