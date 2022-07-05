ff370eee3f9e9d922e404d83c524d0d007e49c01
captswag <iamanjithsasindran@gmail.com>
2022-07-05 18:48:39 +0530

    Leetcode (update) - 1480. Running Sum of 1d Array

--- src/main/kotlin/leetcode/_1480/Solution.kt
+++ src/main/kotlin/leetcode/_1480/Solution.kt
@@ -2,17 +2,9 @@ package leetcode._1480
 
 class Solution {
     fun runningSum(nums: IntArray): IntArray {
-        var index = 0
-
-        var currentSum = nums[index]
-        index++
-
-        while (index < nums.size) {
-            nums[index] = nums[index] + currentSum
-            currentSum = nums[index]
-            index++
+        for (index in 1 until nums.size) {
+            nums[index] = nums[index - 1] + nums[index]
         }
-
         return nums
     }
 }
