528cf1179c034f60504f42597cd8b4e845323bd4
captswag <iamanjithsasindran@gmail.com>
2023-05-04 13:44:36 +0530

    (Update) Implement Binary Search

--- src/main/kotlin/basics/arrays/binarysearch/Solution.kt
+++ src/main/kotlin/basics/arrays/binarysearch/Solution.kt
@@ -7,8 +7,17 @@ class Solution {
         var high = nums.size - 1
         var searchIndex = -1
 
-        while (low < high) {
-            val mid = (low + high) / 2
+        while (low <= high) {
+            /**
+             * https://ai.googleblog.com/2006/06/extra-extra-read-all-about-it-nearly.html
+             * mid =(low + high) / 2
+             * On the face of it, this assertion might appear correct, but it fails for large values of the int
+             * variables low and high. Specifically, it fails if the sum of low and high is greater than the maximum
+             * positive int value (231 - 1). The sum overflows to a negative value, and the value stays negative when
+             * divided by two. In C this causes an array index out of bounds with unpredictable results. In Java, it
+             * throws ArrayIndexOutOfBoundsException.
+             */
+            val mid = (low + high) ushr 1 // Could also use low + ((high - low) / 2)
 
             if (nums[mid] == x) {
                 searchIndex = mid
