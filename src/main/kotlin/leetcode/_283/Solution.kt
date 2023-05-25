c8bf366fd99f725bd7c602aef352d41e0a09a40d
captswag <iamanjithsasindran@gmail.com>
2023-05-26 02:13:00 +0530

    Leetcode (update) - 283. Move Zeroes

--- src/main/kotlin/leetcode/_283/Solution.kt
+++ src/main/kotlin/leetcode/_283/Solution.kt
@@ -1,20 +1,15 @@
 package leetcode._283
 
-import extentions.displayFormatted
-
 class Solution {
     fun moveZeroes(nums: IntArray): Unit {
-        var current = 0
-        var previous = 0 // This will be previous as we move on with the iteration (as long as there are zeroes)
-        while (current < nums.size) {
-            if (nums[current] != 0) {
-                val temp = nums[previous]
-                nums[previous] = nums[current]
-                nums[current] = temp
-                previous++
+        var first = 0
+        for (index in nums.indices) {
+            if (nums[index] != 0) {
+                val temp = nums[first]
+                nums[first] = nums[index]
+                nums[index] = temp
+                first++
             }
-            current++
         }
-        nums.displayFormatted()
     }
 }
