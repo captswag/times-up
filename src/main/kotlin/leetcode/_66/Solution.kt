25e22b007378232cf5774e7821dd6a48a6c5818d
captswag <iamanjithsasindran@gmail.com>
2023-05-26 12:57:13 +0530

    Leetcode (update) - 66. Plus One

--- src/main/kotlin/leetcode/_66/Solution.kt
+++ src/main/kotlin/leetcode/_66/Solution.kt
@@ -2,44 +2,30 @@ package leetcode._66
 
 class Solution {
     fun plusOne(digits: IntArray): IntArray {
-        var lastIndex = digits.size - 1 // Given that the digits array is not empty
-        var firstTime = true
-        var carryForward = 0
-
-        while (lastIndex >= 0) {
-            val digit = digits[lastIndex]
-
-            val sum: Int
-            if (firstTime) {
-                sum = digit + 1
-                firstTime = false
-            } else {
-                sum = digit + carryForward
+        var all9 = true
+        for (digit in digits) {
+            if (digit != 9) {
+                all9 = false
             }
-
-            val value: Int
-            if (sum == 10) {
-                value = 0
-                carryForward = 1
-            } else {
-                value = sum
-                carryForward = 0
-            }
-
-            digits[lastIndex] = value
-            lastIndex--
         }
 
-        return if (carryForward == 1) {
-            val updatedOutput = IntArray(digits.size + 1)
-            updatedOutput[0] = 1
-
-            for (index in digits.indices) {
-                updatedOutput[index + 1] = digits[index]
-            }
-            updatedOutput
+        val output = if (all9) {
+            IntArray(digits.size + 1)
         } else {
-            digits
+            IntArray(digits.size)
         }
+
+        var number = 1
+        for (index in digits.size - 1 downTo 0) {
+            val sum = digits[index] + number
+            output[index] = sum % 10
+            number = sum / 10
+        }
+
+        if (all9) {
+            output[0] = number
+        }
+
+        return output
     }
 }
