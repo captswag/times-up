070672206de799c09a85877740f4036166c7377d
captswag <iamanjithsasindran@gmail.com>
2023-05-26 12:10:12 +0530

    Leetcode (update) - 415. Add Strings

--- src/main/kotlin/leetcode/_415/Solution.kt
+++ src/main/kotlin/leetcode/_415/Solution.kt
@@ -4,37 +4,34 @@ class Solution {
     fun addStrings(num1: String, num2: String): String {
         var lastIndex1 = num1.length - 1
         var lastIndex2 = num2.length - 1
+        var carryOver = 0
+        val sb = StringBuilder()
 
-        var output = ""
-        var carryForward = 0
-        while (lastIndex1 >= 0 || lastIndex2 >= 0) {
-            val digit1 = findDigit(num1, lastIndex1)
-            val digit2 = findDigit(num2, lastIndex2)
+        while (lastIndex1 >= 0 && lastIndex2 >= 0) {
+            val number1 = getDigit(num1, lastIndex1)
+            val number2 = getDigit(num2, lastIndex2)
+            var sum = number1 + number2 + carryOver
 
-            var sum = digit1 + digit2 + carryForward
-            if (sum > 9) {
-                sum %= 10
-                carryForward = 1
-            } else {
-                carryForward = 0
-            }
-
-            output = "$sum" + output
+            carryOver = sum / 10
+            sum %= 10
+            sb.insert(0, sum)
 
             lastIndex1--
             lastIndex2--
         }
 
-        if (carryForward > 0) {
-            output = "$carryForward" + output
+        if (carryOver != 0) {
+            sb.append(0, carryOver)
         }
 
-        return output
+        return String(sb)
     }
 
-    private fun findDigit(num: String, index: Int): Int = if (index >= 0) {
-        Character.getNumericValue(num[index])
-    } else {
-        0
+    private fun getDigit(num: String, index: Int): Int {
+        return if (index < 0) {
+            0
+        } else {
+            Character.getNumericValue(num[index])
+        }
     }
 }
