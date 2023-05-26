09d4946b39ef9d99eba9bc083fc6e547704bc98b
captswag <iamanjithsasindran@gmail.com>
2023-05-26 18:41:29 +0530

    Leetcode (update) - 1133. Largest Unique Number

--- src/main/kotlin/leetcode/_1133/Solution.kt
+++ src/main/kotlin/leetcode/_1133/Solution.kt
@@ -5,21 +5,13 @@ class Solution {
         val map = mutableMapOf<Int, Int>()
 
         for (num in nums) {
-            val count = map[num]
-            if (count == null) {
-                map[num] = 1
-            } else {
-                map[num] = count + 1
-            }
+            map[num] = map.getOrDefault(num, 0) + 1
         }
 
         var output = -1
-        for (key in map.keys) {
-            val count = map[key]
-            if (count != null && count == 1) {
-                if (key > output) {
-                    output = key
-                }
+        for (num in nums) {
+            if (map.getOrDefault(num, 0) == 1) {
+                output = Math.max(num, output)
             }
         }
 
