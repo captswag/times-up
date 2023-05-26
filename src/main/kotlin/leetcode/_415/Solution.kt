7da24d3400a5da477d238ee2e49585cab79f4663
captswag <iamanjithsasindran@gmail.com>
2023-05-26 12:38:11 +0530

    Leetcode (update) - 415. Add Strings

--- src/main/kotlin/leetcode/_415/Solution.kt
+++ src/main/kotlin/leetcode/_415/Solution.kt
@@ -2,29 +2,29 @@ package leetcode._415
 
 class Solution {
     fun addStrings(num1: String, num2: String): String {
+        val output = StringBuilder()
+
         var lastIndex1 = num1.length - 1
         var lastIndex2 = num2.length - 1
         var carryOver = 0
-        val sb = StringBuilder()
 
-        while (lastIndex1 >= 0 && lastIndex2 >= 0) {
+        while (lastIndex1 >= 0 || lastIndex2 >= 0) {
             val number1 = getDigit(num1, lastIndex1)
             val number2 = getDigit(num2, lastIndex2)
-            var sum = number1 + number2 + carryOver
+            val sum = number1 + number2 + carryOver
 
+            output.append(sum % 10)
             carryOver = sum / 10
-            sum %= 10
-            sb.insert(0, sum)
 
             lastIndex1--
             lastIndex2--
         }
 
         if (carryOver != 0) {
-            sb.append(0, carryOver)
+            output.append(carryOver)
         }
 
-        return String(sb)
+        return String(output.reverse())
     }
 
     private fun getDigit(num: String, index: Int): Int {
