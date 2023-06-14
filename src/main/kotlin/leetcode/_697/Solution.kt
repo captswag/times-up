99a8efdb87120b4667b9fbddd8e4ff8743319305
captswag <iamanjithsasindran@gmail.com>
2023-06-15 01:13:52 +0530

    Leetcode (update) - 697. Degree of an Array

--- src/main/kotlin/leetcode/_697/Solution.kt
+++ src/main/kotlin/leetcode/_697/Solution.kt
@@ -2,41 +2,33 @@ package leetcode._697
 
 class Solution {
     fun findShortestSubArray(nums: IntArray): Int {
-        val countMap = mutableMapOf<Int, Int>()
-        val leftMap = mutableMapOf<Int, Int>()
-        val rightMap = mutableMapOf<Int, Int>()
+        val map = mutableMapOf<Int, Int>()
+        var maxDegree = 0
 
-        for (index in nums.indices) {
-            countMap[nums[index]] = countMap.getOrDefault(nums[index], 0) + 1
-
-            val left = leftMap[nums[index]]
-            if (left == null) {
-                leftMap[nums[index]] = index
-            } else {
-                rightMap[nums[index]] = index
-            }
+        for (num in nums) {
+            val currDegree = map.getOrDefault(num, 0) + 1
+            map[num] = currDegree
+            maxDegree = Math.max(maxDegree, currDegree)
         }
 
-        var maxCount = -1
-        var output = 1
+        var minLength = Integer.MAX_VALUE
+        for (key in map.keys) {
+            if (map.getValue(key) == maxDegree) {
+                var left = 0
+                var right = nums.size - 1
 
-        for (key in countMap.keys) {
-            val count = countMap[key]
-            if (count != null && count > 1 && count >= maxCount) {
-                val left = leftMap[key]
-                val right = rightMap[key]
-
-                if (left != null && right != null) {
-                    val diff = right - left
-                    if (count > maxCount || diff < output) {
-                        output = diff + 1
-                    }
+                while (nums[left] != key) {
+                    left++
                 }
 
-                maxCount = count
+                while (nums[right] != key) {
+                    right--
+                }
+
+                minLength = Math.min(minLength, right - left + 1)
             }
         }
 
-        return output
+        return minLength
     }
 }
