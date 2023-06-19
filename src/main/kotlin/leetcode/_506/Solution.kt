ad5b7e104973f771d7fafba054d8b59812842233
captswag <iamanjithsasindran@gmail.com>
2023-06-19 15:38:30 +0530

    Leetcode (update) - 506. Relative Ranks

--- src/main/kotlin/leetcode/_506/Solution.kt
+++ src/main/kotlin/leetcode/_506/Solution.kt
@@ -1,33 +1,26 @@
 package leetcode._506
 
+import java.util.*
+
 class Solution {
     fun findRelativeRanks(score: IntArray): Array<String> {
-        val output = Array(score.size) { "" } // Initialising the Array of size score.size with ""
+        val rankArray = Array(score.size) { "" }
+        val queue = PriorityQueue<Int> { a, b -> score[b] - score[a] }
 
-        val sortedScore = score.sortedArrayDescending()
-        val map = mutableMapOf<Int, String>()
+        for (index in score.indices) {
+            queue.add(index)
+        }
 
         var index = 0
-        while (index < score.size) {
-            val rank = index + 1
-            map[sortedScore[index]] = if (rank == 1) {
-                "Gold Medal"
-            } else if (rank == 2) {
-                "Silver Medal"
-            } else if (rank == 3) {
-                "Bronze Medal"
-            } else {
-                "$rank"
+        while (queue.isNotEmpty()) {
+            rankArray[queue.remove()] = when (++index) {
+                1 -> "Gold Medal"
+                2 -> "Silver Medal"
+                3 -> "Bronze Medal"
+                else -> "$index"
             }
-            index++
         }
 
-        index = 0
-        while (index < score.size) {
-            output[index] = map[score[index]] ?: ""
-            index++
-        }
-
-        return output
+        return rankArray
     }
 }
