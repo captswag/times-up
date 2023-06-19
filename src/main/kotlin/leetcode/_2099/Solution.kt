572769998032c34b05c9aac2c5c9449718f7657d
captswag <iamanjithsasindran@gmail.com>
2023-06-19 17:00:16 +0530

    Leetcode (update) - 2099. Find Subsequence of Length K With the Largest Sum

--- src/main/kotlin/leetcode/_2099/Solution.kt
+++ src/main/kotlin/leetcode/_2099/Solution.kt
@@ -4,36 +4,25 @@ import java.util.*
 
 class Solution {
     fun maxSubsequence(nums: IntArray, k: Int): IntArray {
-        val queue = PriorityQueue<Int>()
-        for (num in nums) {
-            queue.offer(num)
-            if (queue.size > k) {
-                queue.poll()
-            }
-        }
+        val queue = PriorityQueue<Int> { a, b -> nums[a] - nums[b] }
 
-        val map = mutableMapOf<Int, Int>()
-        while (queue.isNotEmpty()) {
-            val value = queue.poll()
-            map[value] = map.getOrDefault(value, 0) + 1
-        }
-
-        val output = IntArray(k)
-        var kIndex = 0
         for (index in nums.indices) {
-            if (nums[index] in map.keys) {
-                val count = map[nums[index]]
-                if (count != null) {
-                    if (count == 1) {
-                        map.remove(nums[index])
-                    } else {
-                        map[nums[index]] = count - 1
-                    }
-                }
-                output[kIndex] = nums[index]
-                kIndex++
+            queue.add(index)
+            if (queue.size > k) {
+                queue.remove()
             }
         }
-        return output
+
+        val maxArray = IntArray(k)
+        for (index in maxArray.indices) {
+            maxArray[index] = queue.remove()
+        }
+        maxArray.sort()
+
+        for (index in maxArray.indices) {
+            maxArray[index] = nums[maxArray[index]]
+        }
+
+        return maxArray
     }
 }
