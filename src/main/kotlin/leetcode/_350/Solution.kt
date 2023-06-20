f8992ffb055422cc37e6007fe3907e3f33795263
captswag <iamanjithsasindran@gmail.com>
2023-06-21 01:24:43 +0530

    Leetcode (update) - 350. Intersection of Two Arrays II

--- src/main/kotlin/leetcode/_350/Solution.kt
+++ src/main/kotlin/leetcode/_350/Solution.kt
@@ -2,32 +2,25 @@ package leetcode._350
 
 class Solution {
     fun intersect(nums1: IntArray, nums2: IntArray): IntArray {
-        // First add the first array elements to HashMap
-        val map = mutableMapOf<Int, Int>()
+        val map1 = mutableMapOf<Int, Int>()
 
         for (num in nums1) {
-            val occurrence = map[num]
-            if (occurrence == null) {
-                map[num] = 1
-            } else {
-                map[num] = occurrence + 1
-            }
+            map1[num] = map1.getOrDefault(num, 0) + 1
         }
 
-        val list = mutableListOf<Int>()
-
+        val intersectionList = mutableListOf<Int>()
         for (num in nums2) {
-            val occurrence = map[num]
-            if (occurrence != null) {
-                list.add(num)
-                if (occurrence == 1) {
-                    map.remove(num)
+            if (map1.containsKey(num)) {
+                intersectionList.add(num)
+                val count = map1.getValue(num)
+                if (count == 1) {
+                    map1.remove(num)
                 } else {
-                    map[num] = occurrence - 1
+                    map1[num] = count - 1
                 }
             }
         }
 
-        return list.toIntArray()
+        return intersectionList.toIntArray()
     }
 }
