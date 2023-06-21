d6542f0426be732834c291b85eeaa30a12dd643b
captswag <iamanjithsasindran@gmail.com>
2023-06-21 23:51:36 +0530

    Leetcode (update) - 383. Ransom Note

--- src/main/kotlin/leetcode/_383/Solution.kt
+++ src/main/kotlin/leetcode/_383/Solution.kt
@@ -2,30 +2,22 @@ package leetcode._383
 
 class Solution {
     fun canConstruct(ransomNote: String, magazine: String): Boolean {
-        val map = mutableMapOf<Char, Int>()
-        var output = true
+        var canConstruct = true
+        val magazineCharCount = IntArray(26)
 
         for (char in magazine) {
-            val count = map[char]
-            if (count == null) {
-                map[char] = 1
-            } else {
-                map[char] = count + 1
-            }
+            magazineCharCount[char - 'a']++
         }
 
         for (char in ransomNote) {
-            val count = map[char]
-            if (count == null) {
-                output = false
+            val charIndex = char - 'a'
+            magazineCharCount[charIndex]--
+            if (magazineCharCount[charIndex] < 0) {
+                canConstruct = false
                 break
-            } else if (count > 1) {
-                map[char] = count - 1
-            } else {
-                map.remove(char)
             }
         }
 
-        return output
+        return canConstruct
     }
 }
