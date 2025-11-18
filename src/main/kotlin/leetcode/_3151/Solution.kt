3ec849c97a6436c3ef366d4580507f8193ec3aa6
captswag <iamanjithsasindran@gmail.com>
2025-11-18 13:38:53 +0530

    Leetcode (update) - 3151. Special Array I

--- src/main/kotlin/leetcode/_3151/Solution.kt
+++ src/main/kotlin/leetcode/_3151/Solution.kt
@@ -5,19 +5,10 @@ class Solution {
     fun isArraySpecial(nums: IntArray): Boolean {
         var output = true
 
-        for (index in 0 until nums.size - 2) {
-            val flag = nums[index] % 2 == 0
-
-            if (nums[index + 1] % 2 == 0) {
-                if (flag) {
-                    output = false
-                    break
-                }
-            } else {
-                if (!flag) {
-                    output = false
-                    break
-                }
+        for (index in 0 until nums.size - 1) {
+            if (nums[index] % 2 == nums[index + 1] % 2) {
+                output = false
+                break
             }
         }
 
