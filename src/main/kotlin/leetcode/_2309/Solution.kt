7bb728106f6d7b93d8a56f622f24efbd83a51906
captswag <iamanjithsasindran@gmail.com>
2025-12-04 21:54:02 +0530

    Leetcode (update) - 2309. Greatest English Letter in Upper and Lower Case

--- src/main/kotlin/leetcode/_2309/Solution.kt
+++ src/main/kotlin/leetcode/_2309/Solution.kt
@@ -2,33 +2,22 @@ package leetcode._2309
 
 class Solution {
     fun greatestLetter(s: String): String {
-        val countArray = IntArray(26)
+        val seen = BooleanArray(52)
 
         for (c in s) {
-            if (c.isUpperCase()) {
-                if (countArray[c - 'A'] == 0) {
-                    countArray[c - 'A'] = 1
-                } else if (countArray[c - 'A'] == 2) {
-                    countArray[c - 'A'] = 3
-                }
+            if (c.isLowerCase()) {
+                seen[c - 'a'] = true
             } else {
-                // Lowercase
-                if (countArray[c - 'a'] == 0) {
-                    countArray[c - 'a'] = 2
-                } else if (countArray[c - 'a'] == 1) {
-                    countArray[c - 'a'] = 3
-                }
+                seen[c - '\''] = true
             }
         }
 
-        var output = ""
-        for (index in countArray.size - 1 downTo 0) {
-            if (countArray[index] == 3) {
-                output = "${(index + 65).toChar()}"
-                break
+        for (index in seen.size - 1 downTo 26) {
+            if (seen[index] && seen[index - 26]) {
+                return (index + 39).toChar().toString()
             }
         }
 
-        return output
+        return ""
     }
 }
