2a1ca7ff0c6dbd1846a33756a578e3dcf19927ff
captswag <iamanjithsasindran@gmail.com>
2025-12-06 00:31:22 +0530

    Leetcode (update) - 125. Valid Palindrome

--- src/main/kotlin/leetcode/_125/Solution.kt
+++ src/main/kotlin/leetcode/_125/Solution.kt
@@ -2,31 +2,21 @@ package leetcode._125
 
 class Solution {
     fun isPalindrome(s: String): Boolean {
-        val sb = StringBuilder()
-        for (c in s) {
-            if (c.isUpperCase()) {
-                sb.append(c.toLowerCase())
-            } else if (c.isLetterOrDigit()) {
-                sb.append(c)
+        var left = 0
+        var right = s.length - 1
+
+        while (left < right) {
+            if (s[left].isLetterOrDigit() && s[right].isLetterOrDigit()) {
+                if (s[left++].lowercaseChar() != s[right--].lowercaseChar()) {
+                    return false
+                }
+            } else if (s[left].isLetterOrDigit()) {
+                right--
+            } else {
+                left++
             }
         }
 
-        return checkIfPalindrome(sb.toString())
-    }
-
-    private fun checkIfPalindrome(s: String): Boolean {
-        var startIndex = 0
-        var endIndex = s.length - 1
-        var palindrome = true
-
-        while (startIndex < endIndex) {
-            if (s[startIndex] != s[endIndex]) {
-                palindrome = false
-                break
-            }
-            startIndex++
-            endIndex--
-        }
-        return palindrome
+        return true
     }
 }
