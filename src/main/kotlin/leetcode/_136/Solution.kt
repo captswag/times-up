d9673c5f970962fda046901f0fa3eb04a3230aa7
captswag <iamanjithsasindran@gmail.com>
2025-12-06 00:14:50 +0530

    Leetcode (update) - 136. Single Number

--- src/main/kotlin/leetcode/_136/Solution.kt
+++ src/main/kotlin/leetcode/_136/Solution.kt
@@ -3,12 +3,12 @@ package leetcode._136
 class Solution {
 
     fun singleNumber(nums: IntArray): Int {
-        var output = 0
+        var singleNumber = 0
 
         for (num in nums) {
-            output = output xor num
+            singleNumber = singleNumber xor num
         }
 
-        return output
+        return singleNumber
     }
 }
