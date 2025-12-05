1ff3efa558298fc892f0e5a0528396ad1e2b9526
captswag <iamanjithsasindran@gmail.com>
2025-12-05 23:28:46 +0530

    Leetcode (update) - 242. Valid Anagram

--- src/main/kotlin/leetcode/_242/Solution.kt
+++ src/main/kotlin/leetcode/_242/Solution.kt
@@ -2,35 +2,22 @@ package leetcode._242
 
 class Solution {
     fun isAnagram(s: String, t: String): Boolean {
-        val map = mutableMapOf<Char, Int>()
+        val frequency = IntArray(26)
 
         for (c in s) {
-            val count = map[c]
-            if (count == null) {
-                map[c] = 1
-            } else {
-                map[c] = count + 1
-            }
+            frequency[c - 'a']++
         }
-
-        var output = true
 
         for (c in t) {
-            val count = map[c]
-            if (count == null) {
-                output = false
-                break
-            } else if (count == 1) {
-                map.remove(c)
-            } else {
-                map[c] = count - 1
+            frequency[c - 'a']--
+        }
+
+        for (f in frequency) {
+            if (f != 0) {
+                return false
             }
         }
 
-        if (map.isNotEmpty()) {
-            output = false
-        }
-
-        return output
+        return true
     }
 }
