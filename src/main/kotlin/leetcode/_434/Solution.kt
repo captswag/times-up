f96b06c6361f26e4eff72807292c977c86a2a37f
captswag <iamanjithsasindran@gmail.com>
2025-12-06 13:03:00 +0530

    Leetcode (update) - 434. Number of Segments in a String

--- src/main/kotlin/leetcode/_434/Solution.kt
+++ src/main/kotlin/leetcode/_434/Solution.kt
@@ -2,22 +2,21 @@ package leetcode._434
 
 class Solution {
     fun countSegments(s: String): Int {
-        var startWord = false
-        var count = 0
-        for (c in s) {
-            if (c == ' ') {
-                if (startWord) {
-                    startWord = false
-                    count++
-                }
+        var segments = 0
+        var index = 0
+
+        while (index < s.length) {
+            if (s[index] == ' ') {
+                index++
             } else {
-                startWord = true
+                segments++
+                index++
+                while (index < s.length && s[index] != ' ') {
+                    index++
+                }
             }
         }
-        return if (startWord) {
-            count + 1
-        } else {
-            count
-        }
+
+        return segments
     }
 }
