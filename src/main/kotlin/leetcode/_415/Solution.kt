bcb07f6bde649388fc8f761bd2aa0d0787a12e77
captswag <iamanjithsasindran@gmail.com>
2025-12-06 21:38:21 +0530

    Leetcode (update) - 415. Add Strings

--- src/main/kotlin/leetcode/_415/Solution.kt
+++ src/main/kotlin/leetcode/_415/Solution.kt
@@ -2,36 +2,34 @@ package leetcode._415
 
 class Solution {
     fun addStrings(num1: String, num2: String): String {
-        val output = StringBuilder()
-
-        var lastIndex1 = num1.length - 1
-        var lastIndex2 = num2.length - 1
+        val sum = CharArray(Math.max(num1.length, num2.length) + 1)
+        var index1 = num1.length - 1
+        var index2 = num2.length - 1
+        var indexSum = sum.size - 1
         var carryOver = 0
 
-        while (lastIndex1 >= 0 || lastIndex2 >= 0) {
-            val number1 = getDigit(num1, lastIndex1)
-            val number2 = getDigit(num2, lastIndex2)
-            val sum = number1 + number2 + carryOver
-
-            output.append(sum % 10)
-            carryOver = sum / 10
-
-            lastIndex1--
-            lastIndex2--
+        while (index1 >= 0 || index2 >= 0) {
+            val currSum = getDigit(num1, index1) + getDigit(num2, index2) + carryOver
+            carryOver = currSum / 10
+            sum[indexSum] = ((currSum % 10) + 48).toChar()
+            index1--
+            index2--
+            indexSum--
         }
 
-        if (carryOver != 0) {
-            output.append(carryOver)
+        return if (carryOver == 1) {
+            sum[indexSum] = (carryOver + 48).toChar()
+            String(sum)
+        } else {
+            String(sum, 1, sum.size - 1)
         }
-
-        return String(output.reverse())
     }
 
     private fun getDigit(num: String, index: Int): Int {
         return if (index < 0) {
             0
         } else {
-            Character.getNumericValue(num[index])
+            num[index] - '0'
         }
     }
 }
