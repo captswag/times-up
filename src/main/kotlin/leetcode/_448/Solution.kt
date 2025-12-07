fc7739ce9cbdb8df82d769886ccfb63b2d296206
captswag <iamanjithsasindran@gmail.com>
2025-12-07 20:24:30 +0530

    Leetcode (update) - 448. Find All Numbers Disappeared in an Array

--- src/main/kotlin/leetcode/_448/Solution.kt
+++ src/main/kotlin/leetcode/_448/Solution.kt
@@ -3,28 +3,18 @@ package leetcode._448
 class Solution {
     fun findDisappearedNumbers(nums: IntArray): List<Int> {
         val set = mutableSetOf<Int>()
-        val high = nums.size
-        var missing = false
 
         for (num in nums) {
-            if (set.contains(num)) {
-                missing = true
-            }
             set.add(num)
         }
 
-        val output = mutableListOf<Int>()
-
-        for (num in 1..high) {
-            if (!set.contains(num)) {
-                output.add(num)
+        val missing = mutableListOf<Int>()
+        for (num in 1..nums.size) {
+            if (num !in set) {
+                missing.add(num)
             }
         }
 
-        if (output.isEmpty() && missing) {
-            output.add(high + 1)
-        }
-
-        return output
+        return missing
     }
 }
