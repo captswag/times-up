36cb355c882b1ce1a91cd1846040409e04cf62ca
captswag <iamanjithsasindran@gmail.com>
2025-12-16 21:18:59 +0530

    Leetcode (refresh) - 509. Fibonacci Number

---
+++ src/main/kotlin/refresh/_509/Solution.kt
@@ -0,0 +1,19 @@
+package refresh._509
+
+class Solution {
+
+    fun fib(n: Int): Int {
+        return if (n < 2) {
+            n
+        } else {
+            var a = 0
+            var b = 1
+            for (currN in 2..n) {
+                val next = b + a
+                a = b
+                b = next
+            }
+            b
+        }
+    }
+}
