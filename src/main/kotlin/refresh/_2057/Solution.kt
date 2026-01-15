5ed2a49c6bf3750d4d4d2c70eb1305210eeea15e
captswag <iamanjithsasindran@gmail.com>
2026-01-16 00:26:47 +0530

    Leetcode (refresh) - 2057. Smallest Index With Equal Value

---
+++ src/main/kotlin/refresh/_2057/Solution.kt
@@ -0,0 +1,13 @@
+package refresh._2057
+
+class Solution {
+
+    fun smallestEqual(nums: IntArray): Int {
+        for (i in nums.indices) {
+            if ((i % 10) == nums[i]) {
+                return i
+            }
+        }
+        return -1
+    }
+}
