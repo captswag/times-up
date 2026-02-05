70fe6f10786ceb8c1d33d27c099ef9702c5cd9b1
captswag <iamanjithsasindran@gmail.com>
2026-02-06 02:17:45 +0530

    Leetcode (refresh, update) - 3038. Maximum Number of Operations With the Same Score I

--- src/main/kotlin/refresh/_3038/Solution.kt
+++ src/main/kotlin/refresh/_3038/Solution.kt
@@ -3,9 +3,10 @@ package refresh._3038
 class Solution {
 
     fun maxOperations(nums: IntArray): Int {
-        val score = nums[0] + nums[1]
-        var i = 2
+        var i = 0
+        val score = nums[i] + nums[++i]
         var cnt = 1
+        i++
 
         while (i + 1 < nums.size && nums[i] + nums[i + 1] == score) {
             cnt++
