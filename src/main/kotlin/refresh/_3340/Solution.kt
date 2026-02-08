e45c70896607b1d9cc13d94f8fe006f7768da853
captswag <iamanjithsasindran@gmail.com>
2026-02-08 17:17:54 +0530

    Leetcode (refresh) - 3340. Check Balanced String

---
+++ src/main/kotlin/refresh/_3340/Solution.kt
@@ -0,0 +1,19 @@
+package refresh._3340
+
+class Solution {
+
+    fun isBalanced(num: String): Boolean {
+        var sum1 = 0
+        var sum2 = 0
+
+        for (i in num.indices) {
+            if (i and 1 == 0) {
+                sum1 += (num[i] - '0')
+            } else {
+                sum2 += (num[i] - '0')
+            }
+        }
+
+        return sum1 == sum2
+    }
+}
