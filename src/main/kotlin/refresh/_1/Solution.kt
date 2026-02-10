a9a4280c7db3cd22bd8c1f29ff93ecc213b4504e
captswag <iamanjithsasindran@gmail.com>
2026-02-10 13:56:44 +0530

    Leetcode (refresh, update) - 1. Two Sum

--- src/main/kotlin/refresh/_1/Solution.kt
+++ src/main/kotlin/refresh/_1/Solution.kt
@@ -3,16 +3,16 @@ package refresh._1
 class Solution {
 
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        val map = mutableMapOf<Int, Int>()
+        val mp = mutableMapOf<Int, Int>()
         val ans = IntArray(2)
         var i = 0
 
-        while (target - nums[i] !in map.keys) {
-            map[nums[i]] = i
+        while (target - nums[i] !in mp.keys) {
+            mp[nums[i]] = i
             i++
         }
 
-        ans[0] = map.getValue(target - nums[i])
+        ans[0] = mp.getValue(target - nums[i])
         ans[1] = i
 
         return ans
