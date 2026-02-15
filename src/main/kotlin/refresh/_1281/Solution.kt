d8d5dbb717f683a4ecaf6cc166b9a224c385a5f8
captswag <iamanjithsasindran@gmail.com>
2026-02-15 16:02:15 +0530

    Leetcode (refresh, update) - 1281. Subtract the Product and Sum of Digits of an Integer

--- src/main/kotlin/refresh/_1281/Solution.kt
+++ src/main/kotlin/refresh/_1281/Solution.kt
@@ -3,17 +3,17 @@ package refresh._1281
 class Solution {
 
     fun subtractProductAndSum(n: Int): Int {
-        var num = n
-        var product = 1
+        var x = n
+        var prod = 1
         var sum = 0
 
-        while (num != 0) {
-            val mod = num % 10
-            product *= mod
-            sum += mod
-            num /= 10
+        while (x != 0) {
+            val d = x % 10
+            prod *= d
+            sum += d
+            x /= 10
         }
 
-        return product - sum
+        return prod - sum
     }
 }
