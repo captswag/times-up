1ee34ca136edf45d0b9a635c0df0c39e9bcc470b
captswag <iamanjithsasindran@gmail.com>
2026-02-22 21:02:15 +0530

    Leetcode (refresh) - 217. Contains Duplicate

---
+++ src/main/kotlin/refresh/_217/Solution.kt
@@ -0,0 +1,18 @@
+package refresh._217
+
+class Solution {
+
+    fun containsDuplicate(nums: IntArray): Boolean {
+        val set = mutableSetOf<Int>()
+
+        for (num in nums) {
+            if (set.contains(num)) {
+                return true
+            } else {
+                set.add(num)
+            }
+        }
+
+        return false
+    }
+}
