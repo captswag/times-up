a272789cce3f38d2af15d22ad1b0bf3eac551514
captswag <iamanjithsasindran@gmail.com>
2026-02-22 21:24:46 +0530

    Leetcode (refresh) - 344. Reverse String

---
+++ src/main/kotlin/refresh/_344/Solution.kt
@@ -0,0 +1,17 @@
+package refresh._344
+
+class Solution {
+
+    fun reverseString(s: CharArray): Unit {
+        var l = 0
+        var r = s.size - 1
+
+        while (l < r) {
+            val temp = s[l]
+            s[l] = s[r]
+            s[r] = temp
+            l++
+            r--
+        }
+    }
+}
