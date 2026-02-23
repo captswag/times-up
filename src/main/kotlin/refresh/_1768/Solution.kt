6f25d9c64592139fc11f696202d630d515a7bb29
captswag <iamanjithsasindran@gmail.com>
2026-02-24 02:39:16 +0530

    Leetcode (refresh, update) - 1768. Merge Strings Alternately

--- src/main/kotlin/refresh/_1768/Solution.kt
+++ src/main/kotlin/refresh/_1768/Solution.kt
@@ -4,21 +4,21 @@ class Solution {
 
     fun mergeAlternately(word1: String, word2: String): String {
         var i = 0
-        var j = 0
         val res = CharArray(word1.length + word2.length)
-        var resIndex = 0
+        var k = 0
 
-        while (i < word1.length && j < word2.length) {
-            res[resIndex++] = word1[i++]
-            res[resIndex++] = word2[j++]
+        while (i < word1.length && i < word2.length) {
+            res[k++] = word1[i]
+            res[k++] = word2[i]
+            i++
         }
 
         while (i < word1.length) {
-            res[resIndex++] = word1[i++]
+            res[k++] = word1[i++]
         }
 
-        while (j < word2.length) {
-            res[resIndex++] = word2[j++]
+        while (i < word2.length) {
+            res[k++] = word2[i++]
         }
 
         return String(res)
