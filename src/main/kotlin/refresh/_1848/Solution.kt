22f00af17d75308d6a8608ea11ebffe57398de3e
captswag <iamanjithsasindran@gmail.com>
2026-02-26 02:03:07 +0530

    Leetcode (refresh, update) - 1848. Minimum Distance to the Target Element

--- src/main/kotlin/refresh/_1848/Solution.kt
+++ src/main/kotlin/refresh/_1848/Solution.kt
@@ -1,19 +1,21 @@
 package refresh._1848
 
+import kotlin.math.abs
+
 class Solution {
 
     fun getMinDistance(nums: IntArray, target: Int, start: Int): Int {
-        var minDistance = Integer.MAX_VALUE
+        var mn = Integer.MAX_VALUE
 
         for (i in nums.indices) {
             if (nums[i] == target) {
-                val currDistance = Math.abs(i - start)
-                if (currDistance < minDistance) {
-                    minDistance = currDistance
+                val cur = abs(i - start)
+                if (cur < mn) {
+                    mn = cur
                 }
             }
         }
 
-        return minDistance
+        return mn
     }
 }
