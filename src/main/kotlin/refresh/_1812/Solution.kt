84c95382f105fceb76dc92e21ad1006f5b3eb140
captswag <iamanjithsasindran@gmail.com>
2026-02-26 00:28:30 +0530

    Leetcode (refresh, update) - 1812. Determine Color of a Chessboard Square

--- src/main/kotlin/refresh/_1812/Solution.kt
+++ src/main/kotlin/refresh/_1812/Solution.kt
@@ -3,14 +3,6 @@ package refresh._1812
 class Solution {
 
     fun squareIsWhite(coordinates: String): Boolean {
-        val row = 8 - (coordinates[1] - '0')
-        val col = coordinates[0] - 'a'
-
-        return when {
-            row and 1 == 0 && col and 1 == 1 -> false
-            row and 1 == 0 && col and 1 == 0 -> true
-            row and 1 == 1 && col and 1 == 1 -> true
-            else -> false
-        }
+        return ((coordinates[0] - 'a') + (coordinates[1] - '0')) and 1 == 0
     }
 }
