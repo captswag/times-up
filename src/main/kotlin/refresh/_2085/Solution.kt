5195e2356918dec36433483e556b66993ea1122c
captswag <iamanjithsasindran@gmail.com>
2026-03-01 17:25:34 +0530

    Leetcode (refresh, update) - 2085. Count Common Words With One Occurrence

--- src/main/kotlin/refresh/_2085/Solution.kt
+++ src/main/kotlin/refresh/_2085/Solution.kt
@@ -3,26 +3,23 @@ package refresh._2085
 class Solution {
 
     fun countWords(words1: Array<String>, words2: Array<String>): Int {
-        val map1 = mutableMapOf<String, Int>()
-
-        for (word in words1) {
-            map1[word] = map1.getOrDefault(word, 0) + 1
+        val freq1 = mutableMapOf<String, Int>()
+        for (w in words1) {
+            freq1[w] = freq1.getOrDefault(w, 0) + 1
         }
 
-        val map2 = mutableMapOf<String, Int>()
-        for (word in words2) {
-            map2[word] = map2.getOrDefault(word, 0) + 1
+        val freq2 = mutableMapOf<String, Int>()
+        for (w in words2) {
+            freq2[w] = freq2.getOrDefault(w, 0) + 1
         }
 
-        var count = 0
-        for (key in map1.keys) {
-            if (map2.containsKey(key)) {
-                if (map1.getValue(key) == 1 && map2.getValue(key) == 1) {
-                    count++
-                }
+        var cnt = 0
+        for (k in freq1.keys) {
+            if (freq1.getValue(k) == 1 && freq2.containsKey(k) && freq2.getValue(k) == 1) {
+                cnt++
             }
         }
 
-        return count
+        return cnt
     }
 }
