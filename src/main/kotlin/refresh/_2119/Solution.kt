e8beef4d6b70694f20e4d78cc4dc4a19fd442eed
captswag <iamanjithsasindran@gmail.com>
2026-03-01 18:33:11 +0530

    Leetcode (refresh, update) - 2119. A Number After a Double Reversal

--- src/main/kotlin/refresh/_2119/Solution.kt
+++ src/main/kotlin/refresh/_2119/Solution.kt
@@ -3,8 +3,6 @@ package refresh._2119
 class Solution {
 
     fun isSameAfterReversals(num: Int): Boolean {
-        return if (num == 0) {
-            true
-        } else num % 10 != 0
+        return num == 0 || num % 10 != 0
     }
 }
