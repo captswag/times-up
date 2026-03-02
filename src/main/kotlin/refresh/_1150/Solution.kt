e0f70765f433a8c47774b409566f9c1ef42780e8
captswag <iamanjithsasindran@gmail.com>
2026-03-02 17:10:39 +0530

    Leetcode (refresh, update) - 1150. Check If a Number Is Majority Element in a Sorted Array

--- src/main/kotlin/refresh/_1150/Solution.kt
+++ src/main/kotlin/refresh/_1150/Solution.kt
@@ -3,22 +3,26 @@ package refresh._1150
 class Solution {
 
     fun isMajorityElement(nums: IntArray, target: Int): Boolean {
-        var cnt = 1
-        var mx = cnt
-        var mxVal = nums[0]
+        var l = 0
+        var r = nums.size - 1
+        var pos = -1
 
-        for (i in 1 until nums.size) {
-            if (nums[i] == nums[i - 1]) {
-                cnt++
-                if (cnt > mx) {
-                    mx = cnt
-                    mxVal = nums[i]
-                }
+        while (l <= r) {
+            val m = (l + r) ushr 1
+            if (nums[m] == target) {
+                pos = m
+                r = m - 1
+            } else if (nums[m] > target) {
+                r = m - 1
             } else {
-                cnt = 1
+                l = m + 1
             }
         }
 
-        return mx > nums.size / 2 && mxVal == target
+        return if (pos == -1) {
+            false
+        } else {
+            pos + (nums.size / 2) < nums.size && nums[pos + (nums.size / 2)] == target
+        }
     }
 }
