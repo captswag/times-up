708151b0c03ab4759aaa3bfbb40a379fb4c03367
captswag <iamanjithsasindran@gmail.com>
2026-03-04 14:41:26 +0530

    Leetcode - 2729. Check if The Number is Fascinating

---
+++ src/main/kotlin/leetcode/_2729/Solution.kt
@@ -0,0 +1,24 @@
+package leetcode._2729
+
+class Solution {
+
+    fun isFascinating(n: Int): Boolean {
+        val freq = IntArray(10)
+
+        for (i in 1..3) {
+            var x = n * i
+            while (x > 0) {
+                freq[x % 10]++
+                x /= 10
+            }
+        }
+
+        for (i in 1 until freq.size) {
+            if (freq[i] != 1) {
+                return false
+            }
+        }
+
+        return freq[0] == 0
+    }
+}
