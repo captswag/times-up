b190205deaccbaa5bb921f9d66bee3b46edc5c37
captswag <iamanjithsasindran@gmail.com>
2026-03-19 14:45:02 +0530

    Leetcode (refresh, update) - 2404. Most Frequent Even Element

--- src/main/kotlin/refresh/_2404/Solution.kt
+++ src/main/kotlin/refresh/_2404/Solution.kt
@@ -3,19 +3,14 @@ package refresh._2404
 class Solution {
 
     fun mostFrequentEven(nums: IntArray): Int {
-        val freq = mutableMapOf<Int, Int>()
+        val cnt = mutableMapOf<Int, Int>()
         var ans = -1
-        var mx = 0
 
-        for (num in nums) {
-            if (num and 1 == 0) {
-                freq[num] = freq.getOrDefault(num, 0) + 1
-                val cur = freq.getValue(num)
-                if (cur == mx && num < ans) {
-                    ans = num
-                } else if (cur > mx) {
-                    mx = cur
-                    ans = num
+        for (x in nums) {
+            if (x and 1 == 0) {
+                cnt[x] = cnt.getOrDefault(x, 0) + 1
+                if (ans == -1 || cnt.getValue(x) > cnt.getValue(ans) || (cnt.getValue(x) == cnt.getValue(ans) && x < ans)) {
+                    ans = x
                 }
             }
         }
