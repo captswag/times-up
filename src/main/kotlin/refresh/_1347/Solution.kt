ee357dfe1d191fba170d23344918907439180d88
captswag <iamanjithsasindran@gmail.com>
2026-03-20 13:54:11 +0530

    Leetcode (refresh) - 1347. Minimum Number of Steps to Make Two Strings Anagram

---
+++ src/main/kotlin/refresh/_1347/Solution.kt
@@ -0,0 +1,22 @@
+package refresh._1347
+
+class Solution {
+
+    fun minSteps(s: String, t: String): Int {
+        val cnt = IntArray(26)
+
+        for (i in s.indices) {
+            cnt[s[i] - 'a']++
+            cnt[t[i] - 'a']--
+        }
+
+        var ans = 0
+        for (x in cnt) {
+            if (x > 0) {
+                ans += x
+            }
+        }
+
+        return ans
+    }
+}
