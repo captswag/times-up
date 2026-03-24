2ffbb43f9a0e01b23dc6866c022c5d34f79d7ae3
captswag <iamanjithsasindran@gmail.com>
2026-03-24 14:26:31 +0530

    Leetcode (refresh, update) - 1903. Largest Odd Number in String

--- src/main/kotlin/refresh/_1903/Solution.kt
+++ src/main/kotlin/refresh/_1903/Solution.kt
@@ -5,18 +5,10 @@ class Solution {
     fun largestOddNumber(num: String): String {
         var i = num.length - 1
 
-        while (i > -1) {
-            if ((num[i] - '0') and 1 == 1) {
-                break
-            }
+        while (i >= 0 && (num[i] - '0') and 1 == 0) {
             i--
         }
 
-        val c = CharArray(i + 1)
-        for (j in 0 until i + 1) {
-            c[j] = num[j]
-        }
-
-        return String(c)
+        return num.substring(0, i + 1)
     }
 }
