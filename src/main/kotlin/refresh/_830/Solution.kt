8f9e68ff40719c833e4fe903825e9c08d4e626b2
captswag <iamanjithsasindran@gmail.com>
2026-03-30 18:56:38 +0530

    Leetcode (refresh, update) - 830. Positions of Large Groups

--- src/main/kotlin/refresh/_830/Solution.kt
+++ src/main/kotlin/refresh/_830/Solution.kt
@@ -3,24 +3,16 @@ package refresh._830
 class Solution {
 
     fun largeGroupPositions(s: String): List<List<Int>> {
+        var i = 0
         val res = mutableListOf<List<Int>>()
-        var index = 1
 
-        while (index < s.length) {
-            if (s[index] == s[index - 1]) {
-                val start = index - 1
-                index++
-                var count = 2
-                while (index < s.length && s[index] == s[index - 1]) {
-                    count++
-                    index++
-                }
-                if (count >= 3) {
-                    res.add(listOf(start, index - 1))
-                }
-                index++
-            } else {
-                index++
+        while (i < s.length) {
+            val j = i
+            while (i < s.length && s[i] == s[j]) {
+                i++
+            }
+            if (i - j >= 3) {
+                res.add(listOf(j, i - 1))
             }
         }
 
